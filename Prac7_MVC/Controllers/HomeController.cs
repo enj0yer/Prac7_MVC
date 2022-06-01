78cb08ce254a85ac8b7056c191d7ac99cfb3fd53
enj0yer <90058431+enj0yer@users.noreply.github.com>
2022-06-01 20:41:38 +0400

    first commit

---
+++ Prac7_MVC/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prac7_MVC.Models;
+
+namespace Prac7_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        PersonContext db = new PersonContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult Form(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(model);
+                db.SaveChanges();
+                if (model.WillCome.Equals("Yes"))
+                {
+                    ViewBag.Result = "Спасибо за ответ. Приятно, что вы придете.";
+                }
+                else
+                {
+                    ViewBag.Result = "Спасибо за ответ. Жаль, что вас не будет";
+                }
+
+                return View("Result");
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+
+        [HttpGet]
+        public ActionResult CheckAll()
+        {
+            return View(db.People);
+        }
+
+    }
+}
---
+++ Prac7_MVC/Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Prac7_MVC.Models
+{
+    public class Person
+    {
+        [HiddenInput(DisplayValue = false)]
+        [Key]
+        public int Id  { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Минимальная длина 3, максимальная 30 символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Минимальная длина 3, максимальная 30 символов")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Минимальная длина 3, максимальная 30 символов")]
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [RegularExpression("^[+][0-9]{11}$", ErrorMessage = "Неверный формат")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [RegularExpression("^[A-Za-z][A-Za-z0-9_]{2,50}[@][A-Za-z]{1,20}[.][A-Za-z]{1,10}$", ErrorMessage = "Неверный формат")]
+        public string Email { get; set; }
+
+        public string WillCome { get; set; }
+    }
+}
